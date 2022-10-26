fef8e5eef840b705de1ab51c7966fb713c2c3298
lightning <lampteyroland8@hotmail.com>
2022-10-26 12:12:29 +0000

    initial commit

---
+++ feedback/about.php
@@ -0,0 +1,6 @@
+<?php include "inc/header.php"; ?>
+    <h2>About</h2>
+
+    <p class="text-center"> This is a PHP project that allows users to save information from the form into a database and also show the information on the feedback page with the name and email of the person who submitted alongside a time stamp</p>
+
+<?php include "inc/footer.php"; ?>
---
+++ feedback/feedback.php
@@ -0,0 +1,31 @@
+<?php include "inc/header.php";  ?>
+
+
+<?php
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
+?>
+   
+    <h2> Past Feedback</h2>
+
+    <?php if (empty($feedback)): ?>
+    <p class="lead mt3"> No feedbacks available</p>
+
+    <?php endif; ?>
+
+    <?php foreach ($feedback as $item): ?>
+        <div class="card my-3 w-75">
+         <div class="card-body text-center">
+           <?php echo $item['body']; ?>
+             <div class="text-secondary mt-2"></div>
+             By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
+         </div>
+       </div>
+    <?php endforeach; ?>
+
+
+<?php include "inc/footer.php"; ?>
